0244564e752b7434c481b72b373d15ee91a99ac4
kodak0803 <42262492+kodak0803@users.noreply.github.com>
2018-08-10 14:03:17 +0700

    Add files via upload

---
+++ Homework 2.py
@@ -0,0 +1,32 @@
+from turtle import*
+shape ("turtle")
+speed(3)
+fillcolor("green")
+for i in range(4):
+    fd(50)
+    lt(90)
+left(25)
+for i in range(4):
+    fd(80)
+    lt(90)
+left(25)
+for i in range(4):
+    fd(120)
+    lt(90)
+left(25)
+for i in range(4):
+    fd(160)
+    lt(90)
+left(25)
+for i in range(4):
+    fd(200)
+    lt(90)
+left(25)
+for i in range(4):
+    fd(240)
+    lt(90)
+
+
+
+
+mainloop()
