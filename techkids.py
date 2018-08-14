d27022791c85405d6629adf52eb987d25d4933d1
kodak0803 <42262492+kodak0803@users.noreply.github.com>
2018-08-14 14:35:40 +0700

    Add files via upload

---
+++ techkids.py
@@ -0,0 +1,25 @@
+from turtle import *
+shape("arrow")
+speed(3)
+for i in range (6):
+    color("red")
+    fd(110)
+    lt(60)
+for i in range (4):
+    fd(110)
+    lt(90)
+color("blue")
+lt(60)
+fd(105)
+rt(117)
+fd(107)
+rt(122)
+fd(110)
+rt(75)
+for i in range(4):
+    fd(110)
+    rt(72)
+
+
+
+mainloop()
